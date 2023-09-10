d7e53eeef89ba488f1d26e7c938200c41e5ae167
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-09-10 22:13:44 +1200

    Uploaded videos to Youtube and swapped Github links with the Youtube ones.

--- src/components/project_data.js
+++ src/components/project_data.js
@@ -3,7 +3,7 @@ const projectData = [
     title: "Card database website",
     description:
       'A website built with Next.js, Node.js and MongoDB. This website started as a website for the "MHA" card game where users could register and login, search the database for cards and build their decks, however within a few weeks we had to scale-down our project to meet the deadline. This meant the website is now only a card database with a search function and login functionalities.',
-    link: "https://private-user-images.githubusercontent.com/88120195/253852779-a9e97a9e-863c-474f-b474-347cff37bae5.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2ODk1NjgyNzUsIm5iZiI6MTY4OTU2Nzk3NSwicGF0aCI6Ii84ODEyMDE5NS8yNTM4NTI3NzktYTllOTdhOWUtODYzYy00NzRmLWI0NzQtMzQ3Y2ZmMzdiYWU1Lm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA3MTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwNzE3VDA0MjYxNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWIxODE1NDNhYmZhZmJkMDlkMWU4NDAyZmI2YWNmY2IzZmFjNTBkYjI4NWY1ZTBmZTc1NTFlNTE2YjdlYjkxYjAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.FpgCqKf6V0j-AhIqeUxIC9PO9TN-AeggxBD0GhewROY",
+    link: "https://youtu.be/JbZzsTftdrw",
   },
   {
     title: "Card Searcher",
@@ -21,7 +21,7 @@ const projectData = [
     title: "Spotify Playlist Generator",
     description:
       'This is a Python project that uses the Spotify API and Spotipy library to generate 2 different playlists. Calm and Upbeat. This organized my music library into the 2 playlists based on the "dancability" of the song.',
-    link: "https://private-user-images.githubusercontent.com/88120195/238177340-6167da84-dec6-44fe-9408-8ef2a2e7d34a.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2ODk1NTA3OTYsIm5iZiI6MTY4OTU1MDQ5NiwicGF0aCI6Ii84ODEyMDE5NS8yMzgxNzczNDAtNjE2N2RhODQtZGVjNi00NGZlLTk0MDgtOGVmMmEyZTdkMzRhLm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA3MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwNzE2VDIzMzQ1NlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNhMjcwYzI3MzMwYjY5NDVjOGUzOGI3OGYxODUyOGRiOWQ5M2I4Y2Q1MjcwZGQ2MzI2MTAxMGUwNjQ2OTkyOWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.XtVwE8SFVBx5EWVnA7vyVL2Ut-CJQh7Ha-SEX1i1kcU",
+    link: "https://youtu.be/8s0RqRqwOnQ",
   },
 ];
 
