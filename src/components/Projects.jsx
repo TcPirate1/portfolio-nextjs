1fe896cb5bf836fb472621c131468879071ef509
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-11-08 11:40:07 +1300

    Added regex to project component so it uses different fontawesome icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,12 @@ import projectData from "./project_data.js";
 import { random_keyID } from "./random_keyID.js";
 
 const Projects = () => {
+  function youtubeLink(link) {
+    const youtubeRegex = new RegExp(
+      "https://youtu.be/[a-zA-Z0-9_-]{11}|https://www.youtube.com/watch?v=[a-zA-Z0-9_-]{11}"
+    );
+    return youtubeRegex.test(link);
+  }
   return (
     <div className="projects-container">
       <h2>Projects</h2>
@@ -12,9 +18,15 @@ const Projects = () => {
               <div className="project-header">
                 <i className="fa-regular fa-folder-open folder-icon"></i>
                 <div className="small-icons">
-                  <a href={project.link} target="_blank">
-                    <i className="fa-brands fa-square-github fa-2xl"></i>
-                  </a>
+                  {youtubeLink(project.link) ? (
+                    <a href={project.link} target="_blank">
+                      <i className="fa-brands fa-youtube fa-2xl"></i>
+                    </a>
+                  ) : (
+                    <a href={project.link} target="_blank">
+                      <i className="fa-brands fa-square-github fa-2xl"></i>
+                    </a>
+                  )}
                 </div>
               </div>
               <h3>{project.title}</h3>
