9fecfe7b29c5da021980355128c34c89138f3fec
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2025-01-11 14:01:57 +1300

    Created hobbies and fav show list

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,12 @@
+'use client'
 import Image from "next/image";
+import { hobbies } from "./hobbies";
+import { fav_shows } from "./favourite_shows";
 
 const About = () => {
+
+  const hobbyString = hobbies.map(hobby => hobby.name).join(', ')
+
   return (
     <div className="about-container">
       <h2>About Me</h2>
@@ -9,29 +15,37 @@ const About = () => {
           <p>
             I started in 2020, like everyone else, had decided to shift careers.
             After working a couple of jobs I had settled for a forklift driving
-            job for a few years after my first time studying science. I had
-            decided to go back to study and chose IT, not really knowing what I
-            wanted but had experience building computers and at the time, played
-            a lot of video games. This decision came at a convenient time as I
-            found out the NZ government were funding IT studies through TTAF
-            program and gave it a go.
-            <br /> I am now on my last year of the Bachelors and looking forward
-            to seeing what this can lead to!
+            job for a few years after my first time studying science.
+            <br />
+            I had decided to go back to study and chose IT, not really knowing
+            what I wanted. This decision came at a convenient time as I found
+            out the NZ government were funding IT studies through TTAF program
+            and gave it a go.
+            <br /> I have now graduated with my Bachelors and looking forward to
+            seeing what this can lead to!
           </p>
           <p>
-            In my spare time, I play video, card & board games, watch tv shows,
-            anime and read books and finding new ways to mess with my Linux
-            system.
+            My hobbies include {hobbyString}, watching tv shows, and finding new
+            ways to mess with my Linux system.
+            <br />
+            <u>My favourite shows are:</u>
+            <ul>
+              {fav_shows.map((shows, index) => (
+                <li key={index}>- {shows.name}</li>
+              ))}
+            </ul>
           </p>
           <p>
             Throughout my studies, I have been challenged to use a variety of
             languages and frameworks/libraries associated with the language and
             have been able to use the knowledge obtained to create a variety of
-            projects. This ranged from static and dynamic websites to terminal
-            programs and mobile apps and am excited to continue learning. <br />{" "}
-            I have also been able to work in a team environment and have been
-            able to use version control systems such as Git and GitHub to
-            collaborate with my team members.
+            projects.
+            <br />
+            This ranged from static and dynamic websites to terminal programs
+            and mobile apps and am excited to continue learning.
+            <br />I have also been able to work in a team environment and have been able to
+            use version control systems such as Git and GitHub and experience
+            with Kanban Boards and Jira to collaborate with my team members.
           </p>
         </div>
         <div className="about-img">
---
+++ src/components/hobbies.js
@@ -0,0 +1,17 @@
+export const hobbies = [
+  {
+    name: "video games",
+  },
+  {
+    name: "card games",
+  },
+  {
+    name: "gyming",
+  },
+  {
+    name: "reading",
+  },
+  {
+    name: "drawing",
+  },
+];
