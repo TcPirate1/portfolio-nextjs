2f68d1d272f9f9b726ac9e8cd3b66cf51f202203
tcLaptop <88120195+TcPirate1@users.noreply.github.com>
2025-07-19 17:26:54 +1200

    Hcaptcha works but doesn't seem to send email. Check backend is working as intended when able to.

--- src/app/api/contact.js
+++ src/app/api/contact.js
@@ -1,26 +1,37 @@
 import { verify } from 'hcaptcha';
 import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 export async function POST(req) {
-    const { name, email, message, token } = await req.json();
+    const { name, email, message, captchaToken } = await req.json();
 
-    if (!token) {
-        return NextResponse.json({ message: 'No captcha token' }, {status: 400 });
+    if (!captchaToken) {
+        return NextResponse.json({ message: 'No captcha token' }, { status: 400 });
     }
 
-    const { success, 'error-codes': errors } = await verify(
-        process.env.HCAPTCHA_SECRET,
-        token
-    );
-
+    const { success } = await verify(process.env.HCAPTCHA_SECRET, captchaToken);
     if (!success) {
-        return NextResponse.json({
-            message: 'Captcha validation failed',
-            errors
-        }, {
-            status: 401
-        });
+        return NextResponse.json({ message: 'Captcha failed' }, { status: 401 });
     }
 
-    return NextResponse.json({ message: 'Success' });
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.protonmail.ch',
+        port: 587,
+        secure: true
+    });
+
+    try {
+        await transporter.sendMail({
+            from: email,
+            to: process.env.RECEIVE_EMAIL,
+            subject: `Message from: ${name}`,
+            text: message,
+            html: message,
+            replyTo: email
+        });
+        return NextResponse.json({ message: 'Email sent!' });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: 'Email failed to send'}, { status: 500 });
+    }
 }
