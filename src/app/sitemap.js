524ab859d0f2b39fabc485a40f91c7ee9143a239
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-08-05 12:40:41 +1200

    Add sitemap and change robots.txt to correct sitemap file

---
+++ src/app/sitemap.js
@@ -0,0 +1,9 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://terences-portfolio.vercel.app',
+            lastModified: new Date(),
+            priority: 1,
+        }
+    ]
+}
